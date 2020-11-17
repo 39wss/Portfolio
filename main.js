063c290b73a09b4b84a7208fd0b8e223b52d71b2
39wss <39wss@users.noreply.github.com>
2020-11-17 20:51:52 +0900

    HTML: aos

--- main.js
+++ main.js
@@ -104,3 +104,7 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+
+// AOS
+AOS.init();
